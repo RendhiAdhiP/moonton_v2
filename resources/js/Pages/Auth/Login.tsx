4c2b8c3be45e22f025c6b8acb2ff83a127f23019
RendhiAdhiP <rendhiap123@gmail.com>
2024-08-18 15:56:52 +0700

    feat: integration sign in

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -1,13 +1,11 @@
+import React from "react";
 import { FormEventHandler } from 'react';
-import Checkbox from '@/Components/Checkbox';
-import GuestLayout from '@/Layouts/GuestLayout';
 import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
-
-export default function Login({ status, canResetPassword }: { status?: string, canResetPassword: boolean }) {
+import Input from "@/Components/TextInput"
+import Label from "@/Components/InputLabel";
+import Button from "@/Components/Button";
+import { Link, Head, useForm } from "@inertiajs/react";
+const Login: React.FC = () => {
     const { data, setData, post, processing, errors, reset } = useForm({
         email: '',
         password: '',
@@ -22,72 +20,94 @@ export default function Login({ status, canResetPassword }: { status?: string, c
         });
     };
 
+
     return (
-        <GuestLayout>
-            <Head title="Log in" />
+        <>
+            <Head title="Login" />
+            <div className="mx-auto max-w-screen min-h-screen bg-black text-white md:px-10 px-3">
+                <div className="fixed top-[-50px] hidden lg:block">
+                    <img src="/images/signup-image.png"
+                        className="hidden laptopLg:block laptopLg:max-w-[450px] laptopXl:max-w-[640px]" alt="" />
+                </div>  
+                <div className="py-24 flex laptopLg:ml-[680px] laptopXl:ml-[870px]">
+                    <div>
+                        <img src="/images/moonton-white.svg" alt="" />
+                        <div className="my-[70px]">
+                            <div className="font-semibold text-[26px] mb-3">
+                                Welcome Back
+                            </div>
+                            <p className="text-base text-[#767676] leading-7">
+                                Explore our new movies and get <br />
+                                the better insight for your life
+                            </p>
+                        </div>
+                        <form className="w-[370px]" onSubmit={submit}>
+                            <div className="flex flex-col gap-6">
+                                <div>
+                                    <Label
+                                        value="Email Address"
+                                    />
+                                    <Input
+                                        type="email"
+                                        name="email"
+                                        placeholder="Email Address"
+                                        value={data.email}
+                                        isFocused={true}
+                                        handleChange={(e) => setData('email', e.target.value)}
+                                        required
 
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+                                    />
+                                    <InputError message={errors.email} className="mt-2" />
 
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
+                                </div>
+                                <div>
+                                    <Label
+                                        value="Password"
+                                    />
+                                    <Input
+                                        type="password"
+                                        name="password"
+                                        placeholder="Password"
+                                        value={data.password}
+                                        handleChange={(e) => setData('password', e.target.value)}
+                                        required
 
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
-                    />
+                                    />
+                                    <InputError message={errors.email} className="mt-2" />
 
-                    <InputError message={errors.email} className="mt-2" />
+
+                                </div>
+                            </div>
+                            <div className="grid space-y-[14px]  mt-[30px]">
+                                {/* <Link href={route('prototype.dashboard')}> */}
+                                <Button
+                                    type="submit"
+                                    variant="primary"
+                                    processing={processing}
+                                >
+                                    <span className="text-base font-semibold">
+                                        Start Watching
+                                    </span>
+                                </Button>
+                                {/* </Link> */}
+                                <Link href={route('register')}>
+                                    <Button
+                                        type="submit"
+                                        variant="light-outline"
+                                    >
+                                        <span className="text-base text-white">
+                                            Create New Account
+                                        </span>
+                                    </Button>
+                                </Link>
+
+                            </div>
+                        </form>
+                    </div>
                 </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
-                        />
-                        <span className="ms-2 text-sm text-gray-600">Remember me</span>
-                    </label>
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
-
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
+            </div>
+        </>
     );
-}
+};
+
+export default Login
